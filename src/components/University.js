eded1ab41ed54f109b0ab511cf9cadbe1dec655d
Sylvia Akoth Omollo <sylvia_omollo@gwu.edu>
2022-12-22 16:15:33 +0300

    removed checkout button

--- src/components/University.js
+++ src/components/University.js
@@ -52,10 +52,10 @@ function University() {
   </div>
 
 
-  <div class="form-check">
+  {/* <div class="form-check">
     <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
     <label class="form-check-label" for="exampleCheck1">Check me out</label>
-  </div>
+  </div> */}
   <button type="submit" class="btn btn-primary">Submit</button>
 </form>
   </div>;
