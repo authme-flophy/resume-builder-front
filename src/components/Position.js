b57af1fe476ad61326dfb4c1827903982ccaf4ac
aisha-said <aisha-said@github.com>
2022-12-28 22:02:05 +0300

    created new branch

--- src/components/Position.js
+++ src/components/Position.js
@@ -1,8 +1,1 @@
-import React from "react";
-import "./Position.scss";
 
-function Position() {
-  return <div>Position</div>;
-}
-
-export default Position;
--- src/components/ProgrammingLanguage.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./ProgrammingLanguage.scss";
-
-function ProgrammingLanguage() {
-  return <div>ProgrammingLanguage</div>;
-}
-
-export default ProgrammingLanguage;
